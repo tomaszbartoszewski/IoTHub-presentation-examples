4b42c6f4877cd163acfebc7238d3c188b6aa1a24
tomasz.bartoszewski <tomasz.bartoszewski@gmail.com>
2019-11-17 20:45:31 +0000

    Add script to delete all config files

---
+++ deleteConfig.ps1
@@ -0,0 +1,6 @@
+rm .\Service\conf.json
+rm .\Service_Just_Id_And_Value\conf.json
+rm .\Service_Send_Command\conf.json
+rm .\Service_Send_Command_Ack\conf.json
+rm .\Device\conf.json
+rm .\Device_Processing_Commands\conf.json
